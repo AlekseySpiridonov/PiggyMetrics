df12b5baee476662599246d853877c74b9bb1e06
Aleksey Spiridonov <aleksey@spiridonov.su>
2018-04-23 19:50:38 +0400

    Sandbox: Incorrect Spring Security for healthcheck

--- account-service/src/main/java/com/piggymetrics/account/AccountApplication.java
+++ account-service/src/main/java/com/piggymetrics/account/AccountApplication.java
@@ -64,7 +64,7 @@ public class AccountApplication extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.antMatchers("/" , "/demo", "/health-check").permitAll()
+				.antMatchers("/" , "/demo").permitAll()
 				.anyRequest().authenticated();
 	}
 }
